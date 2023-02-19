381946f670b9d4e24fe7504c117e2ee594a679cf
Gratix0 <oganisyan.sanya1@mail.ru>
2023-02-19 23:31:02 +0300

    4

--- Note/MainWindow.xaml.cs
+++ Note/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace Note
 {
@@ -24,5 +26,64 @@ namespace Note
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescriptionBox.Text != "" && NameNoteBox.Text != "")
+            {
+                var des = JsonSer.Des();
+                List<string> names = new List<string>();
+
+                if (des.Count != 0 && des.ContainsKey(datePicker1.SelectedDate.ToString()))
+                {
+                    foreach (Note name in des[datePicker1.SelectedDate.ToString()])
+                    {
+                        names.Add(name.name);
+                    }
+                    if (des.ContainsKey(datePicker1.SelectedDate.ToString()) && !names.Contains(NameZamBox.Text))
+                    {
+                        Note newNote = new Note();
+                        newNote.descrption = DescriptionBox.Text;
+                        newNote.name = NameZamBox.Text;
+                        newNote.dateTime = datePicker1.SelectedDate.ToString();
+                        des[datePicker1.SelectedDate.ToString()].Add(newNote);
+                        JsonSer.Ser(des);
+
+                    }
+                }
+                else if (des.Count == 0 || !des.ContainsKey(datePicker1.SelectedDate.ToString()))
+                {
+                    Note newNote = new Note();
+                    newNote.descrption = DescriptionBox.Text;
+                    newNote.name = NameZamBox.Text;
+                    newNote.dateTime = datePicker1.SelectedDate.ToString();
+
+                    List<Note> newDate = new List<Note>();
+                    newDate.Add(newNote);
+                    des.Add(newNote.dateTime, newDate);
+                    JsonSer.Ser(des);
+                }
+                Zametki.ItemsSource = des[datePicker1.SelectedDate.ToString()];
+                DescriptionBox.Text = "";
+                NameZamBox.Text = "";
+
+                var des1 = JsonSer.Des();
+
+                if (des1 != null)
+                {
+                    if (des.Keys.Contains(datePicker1.SelectedDate.ToString()))
+                    {
+                        List<Note> next = des1[datePicker1.SelectedDate.ToString()];
+                        List<string> names1 = new List<string>();
+
+                        foreach (Note n in next)
+                        {
+                            names1.Add(n.name);
+                        }
+
+                        Zametki.ItemsSource = names1;
+                    }
+                }
+            }
     }
 }
